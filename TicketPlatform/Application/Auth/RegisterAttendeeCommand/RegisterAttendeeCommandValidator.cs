15c9087aedaa7351f18f6975c67ac0d85887dd75
Omar Gomaa <mrjmh934@gmail.com>
2026-09-14 01:34:16 +0300

    add Attende Register but not complete yet

---
+++ TicketPlatform/Application/Auth/RegisterAttendeeCommand/RegisterAttendeeCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using Application.IServices;
+using Domain.Shared;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Auth.RegisterAttendeeCommand
+{
+    public class RegisterAttendeeCommandValidator : AbstractValidator<RegisterAttendeeCommand>
+    {
+        public RegisterAttendeeCommandValidator(IIdentityService identityService)
+        {
+            RuleFor(x => x.FName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(Constants.Attendee.FirstNameMaxLength).WithMessage("First name cannot exceed 50 characters.");
+            
+            RuleFor(x => x.LName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(Constants.Attendee.LastNameMaxLength).WithMessage("Last name cannot exceed 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.").MustAsync(identityService.CheckEmailIsUniqueAsync);
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.");
+            
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty().WithMessage("Password confirmation is required.")
+                .MinimumLength(8).WithMessage("Password confirmation must be at least 8 characters long.")
+                .Equal(x => x.Password).WithMessage("Passwords do not match.");
+        
+        }
+
+    }
+}
---
+++ TicketPlatform/Infrastructure/Options/EmailOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Options
+{
+    public class EmailOptions
+    {
+        public string EmailHost { get; set; }
+        public int EmailPort { get; set; }
+        public string EmailUsername { get; set; }
+        public string EmailPassword { get; set; }
+    }
+
+}
