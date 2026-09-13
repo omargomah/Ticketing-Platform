52e0f70e46154f4374343a75f5b62739a2cb18c2
Omar Gomaa <mrjmh934@gmail.com>
2026-09-13 20:14:25 +0300

    Add the AppDbContext and adjust AppSettings.json

--- TicketPlatform/Infrastructure/Persistence/Data/ApplicationDbContext.cs
+++ TicketPlatform/Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 namespace Infrastructure.Persistence.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<Seat> Seats => Set<Seat>();
+        public DbSet<Booking> Bookings => Set<Booking>();
+        public DbSet<Attendee> Attendees => Set<Attendee>();
+        public DbSet<Organizer> Organizers => Set<Organizer>();
+        public DbSet<Preference> Preferences => Set<Preference>();
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
