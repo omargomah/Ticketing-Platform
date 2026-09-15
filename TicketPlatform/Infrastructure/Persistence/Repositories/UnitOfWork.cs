0d61975dd5c1400f5067f8e2c4ed89f87f32c4b7
Omar Gomaa <mrjmh934@gmail.com>
2026-09-15 20:10:50 +0300

    Feat: add unitof work and repository of refreshtoken table to use it

---
+++ TicketPlatform/Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<int> SaveChangesAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+    }
+}
