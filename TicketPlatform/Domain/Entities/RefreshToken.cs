6f49e8fc25e4d9ea5d3f29a5ef96e011c14d9961
Omar Gomaa <mrjmh934@gmail.com>
2026-09-15 20:09:48 +0300

    Feat: make the access token refresh by using refresh token

---
+++ TicketPlatform/Domain/Entities/RefreshToken.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain.Entities
+{
+    public class RefreshToken:Entity
+    {
+        private RefreshToken(Guid id) : base(id) { }
+
+        public string Token { get; private set; } = string.Empty;
+        public DateTime ExpiresOn { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public DateTime? RevokedOn { get; private set; }
+
+        public bool IsExpired => DateTime.UtcNow >= ExpiresOn;
+        public bool IsActive => RevokedOn == null && !IsExpired;
+
+        public Guid UserId { get; private set; }    
+    
+        public static RefreshToken Create(Guid userId, DateTime expiresOn)
+        {
+            return new RefreshToken (Guid.NewGuid())
+            {
+                Token = Guid.NewGuid().ToString(),
+                ExpiresOn = expiresOn,
+                CreatedOn = DateTime.UtcNow,
+                UserId = userId,
+                RevokedOn = null
+                
+            };
+        }
+        public void Revoke()
+        {
+            RevokedOn = DateTime.UtcNow;
+        }
+        
+    }
+}
